24b85dadde0495d7187921e443686bd507ce7608
Ben McCann <322311+benmccann@users.noreply.github.com>
2021-06-02 15:39:57 -0700

    [build site]

--- src/config/env.ts
+++
@@ -1,7 +0,0 @@
-export let dev: boolean;
-export let src: string;
-export let dest: string;
-
-export const set_dev = (_: boolean) => dev = _;
-export const set_src = (_: string) => src = _;
-export const set_dest = (_: string) => dest = _;
